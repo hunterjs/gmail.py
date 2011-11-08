8f258eb249989f2ef12e48bb7537843385091770
Harshavardhan Rangan <hvardhan.r@gmail.com>
2011-11-08 00:18:37 -0500

    added command line parameters
    overrode prompt_user_passwd() of FancyUrlopener in order to accept username and password from the command line

--- gmail.py
+++ gmail.py
@@ -1,12 +1,54 @@
 #!/usr/bin/env python
 
-import urllib
+from urllib import FancyURLopener
 import datetime
+from optparse import OptionParser    
 from xml.etree import ElementTree as ET
 
 
+usage = '%prog [options]'
+parser = OptionParser(usage=usage)
+parser.add_option("-u", "--username", dest="username", help="your gmail username")
+parser.add_option("-p", "--password", dest="password", help="your gmail password")
+parser.add_option("-s", "--summary", action="store_true", dest="summary",
+                  default=False, help="prints the summary text that is \
+                                       generally visible in your gmail inbox \
+                                       [default: %default]")
+parser.add_option("-v", "--verbose", action="store_true", dest="summary",
+                  default=False, help="same as --summary")
+
+(options,args) = parser.parse_args()
+
+
+class FancyURLopenerMod(FancyURLopener):
+    """
+    This overrides the default prompt_user_password, allowing for username and
+    password to be passed from the command line.
+    """
+    def __init__(self, *args, **kwargs):
+        # uncomment this once urllib uses new style classes, or in python 3.0+
+        # super(FancyURLopenerMod, self).__init__(*args, **kwargs)
+
+        # till then this will work, but not in python 3.0+
+        FancyURLopener.__init__(self, *args, **kwargs)
+
+    def prompt_user_passwd(self,host,realm):
+        import getpass
+        try: 
+            if options.username: 
+                user = options.username
+            else: user = raw_input("Enter username for %s at %s: " % (realm,
+                                                                      host))
+            if options.password: passwd = options.password
+            else: passwd = getpass.getpass("Enter password for %s in %s at %s: " %
+                                           (user, realm, host))
+            return user, passwd
+        except KeyboardInterrupt:
+            print
+            return None, None
+
 url = 'https://hvardhan.r@mail.google.com/mail/feed/atom/'
-opener = urllib.FancyURLopener()
+opener = FancyURLopenerMod()
 f = opener.open(url)
 feed = f.read()
 
@@ -34,7 +76,8 @@ def sanitize_datetime(datestring):
     # check date
     today = datetime.datetime.today()
     yesterday = datetime.datetime.today() + datetime.timedelta(0,negative_day)
-    daybefore = datetime.datetime.today() + datetime.timedelta(0,negative_day) + datetime.timedelta(0,negative_day)
+    daybefore = datetime.datetime.today() + datetime.timedelta(0,negative_day) \
+                + datetime.timedelta(0,negative_day)
 
     email_datetime_date = email_datetime.strftime('%F')
     today_date = today.strftime('%F')
@@ -76,6 +119,6 @@ if not count:
 for k in tree.findall(tree[-1].tag):
     sdate, stime = sanitize_datetime(k.find(issued).text)
     print '%s at %s: %s <%s> wrote "%s"' % (sdate, stime, k.find(name).text, k.find(email).text, k.find(title).text)
-    print '\t\tText: %s' % (k.find(summary).text)
+    if options.summary: print '\t\tText: %s' % (k.find(summary).text)
 
 print '\nTotal: %s unread messages\n' % count
